4fb7de4dd5f23fc7ecbcd6ba4b758d5f7167df3f
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-10-28 17:36:19 -0400

    cleaned up comments, mvp met

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import React from "react";
 import TodoList from "./components/TodoComponents/TodoList";
 import TodoForm from "./components/TodoComponents/TodoForm";
 
-// you will need a place to store your state in this component.
-// design `App` to be the parent component of your application.
-// this component is going to take care of state, and any change handlers you need to work with your state
-
 class App extends React.Component {
   constructor() {
     super();
